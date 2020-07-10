da3782a733b94d77fc30a6a0ac82726ba875acd6
johnpherson <phersonj@gmail.com>
2020-07-10 17:59:58 -0600

    update

--- src/Main.java
+++ src/Main.java
@@ -21,25 +21,43 @@ public class Main {
                               {' ','|',' ','|',' '}
                              };
 
-
-
         System.out.println("X, Make a selection of 1-9 on the board");
         Scanner xInput = new Scanner(System.in);
         int xChoice = xInput.nextInt();
 
         switch (xChoice) {
             case 1:
-                gameBoard[0] = 'x';
+                gameBoard[0][0] = 'X';
             break;
             case 2:
-                gameBoard[1] = 'x';
+                gameBoard[0][2] = 'X';
+            break;
+            case 3:
+                gameBoard[0][4] = 'X';
+            break;
+            case 4:
+                gameBoard[2][0] = 'X';
+            break;
+            case 5:
+                gameBoard[2][2] = 'X';
+            break;
+            case 6:
+                gameBoard[2][4] = 'X';
+            break;
+            case 7:
+                gameBoard[4][0] = 'X';
+            break;
+            case 8:
+                gameBoard[4][2] = 'X';
+            break;
+            case 9:
+                gameBoard[4][4] = 'X';
+            break;
         }
 
         printGameBoard(gameBoard);
 
     }
-
-
 
          public static void printGameBoard(char[][] gameBoard) {
              for(char[] row : gameBoard) {
@@ -49,8 +67,6 @@ public class Main {
                 System.out.println();
             }
         }
-
-
 
 };
 
